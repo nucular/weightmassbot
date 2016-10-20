292a2a7ce3ee019193a41b655161f1f243b3c82d
nucular <nucularer+git@gmail.com>
2016-10-20 17:28:01 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,17 @@
+# Weight Mass Bot
+
+Polite suggestions about the correct usage of the term "weight".
+
+Inspired by [@nonbinarybot](https://twitter.com/nonbinarybot) and
+[this Reddit comment](https://www.reddit.com/r/TumblrInAction/comments/58dmte/this_bot_exists/d8zvmml).
+
+## Running
+
+```
+pip3 install peony-twitter
+TWITTER_ACCESS_TOKEN=[your twitter API access token]
+TWITTER_ACCESS_TOKEN_SECRET=[your twitter API access token secret]
+TWITTER_CONSUMER_KEY=[your twitter API consumer key]
+TWITTER_CONSUMER_SECRET=[your twitter API consumer secret]
+python weightmassbot.py
+```
---
+++ weightmassbot.py
@@ -0,0 +1,31 @@
+import asyncio, os, sys
+from peony import PeonyClient
+
+SEARCH_QUERY = "weight kg"
+RETWEET_COMMENT = "You may have implied weight was measured in grams instead of Newtons. Did you mean MASS? If not, have a nice day :)"
+ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY = os.environ["TWITTER_CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["TWITTER_CONSUMER_SECRET"]
+
+async def track(client):
+  async with client.stream.statuses.filter.post(track=SEARCH_QUERY) as stream:
+    async for tweet in stream:
+      if not "text" in tweet:
+        continue
+
+      tweet_url = "https://twitter.com/{}/status/{}".format(
+        tweet["user"]["screen_name"], tweet["id"]
+      )
+      status = RETWEET_COMMENT + " " + tweet_url
+      print(status)
+      await client.api.statuses.update.post(status=status)
+
+if __name__ == "__main__":
+  client = PeonyClient(
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET,
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET
+  )
+  asyncio.get_event_loop().run_until_complete(track(client))
